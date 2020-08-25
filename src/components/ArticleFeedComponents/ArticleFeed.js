b0b90e1242314653910236f959aeaf05e5298249
PazBazak <efratbazak2@gmail.com>
2020-08-25 15:10:47 +0300

    Fixed KeyMapError
    Added unique id to each feed from the JSON object

--- src/components/ArticleFeedComponents/ArticleFeed.js
+++ src/components/ArticleFeedComponents/ArticleFeed.js
@@ -4,6 +4,7 @@ import Feed from "./Feed";
 
 const feeds = [
     {
+        id: 1,
         title: "CoronaVirus might just be the end of the world, scientist claims!",
         image: "https://images.medicinenet.com/images/newsletter/specialty/oleander-plant.jpg",
         website: "website1.com",
@@ -12,6 +13,7 @@ const feeds = [
         summary: "osit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur"
     },
     {
+        id: 2,
         title: "Curcumin might be the cause for cancer in some cancer patients?",
         image: "https://5.imimg.com/data5/AF/EZ/DB/SELLER-8051866/curcumin-95-natural-extract-500x500.jpg",
         website: "website2.com",
@@ -20,6 +22,7 @@ const feeds = [
         summary: "ratione voluptatem sequi nesciunt. Neque porroesciunt. Neque porroesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur"
     },
     {
+        id: 3,
         title: "Do black people with dermatitis live less? new research found that niggers lifespan is shorter!",
         image: "https://d279m997dpfwgl.cloudfront.net/wp/2020/07/Emmanuel-1000x776.jpg",
         website: "website3.com",
@@ -32,6 +35,7 @@ const feeds = [
 const createFeed = (feed) => {
     return (
         <Feed
+        key={feed.id}
         title={feed.title}
         image={feed.image}
         website={feed.website}
