3c65b4fcb5123879ad5c3d97bef306e67c19a4b4
PazBazak <efratbazak2@gmail.com>
2020-08-25 13:05:12 +0300

    Feeds added from JSON
    ArticleFeed components reads a JSON objects and then renders feed objects from the JSOM info (last step before using the API)
    
    added bootstrap.js.bundle to add functional navbar on mobile-size

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
 import Header from "./components/HeaderComponents/Header";
 import DiseasesSlider from "./components/DiseaseSliderComponents/DiseasesSlider";
 import ArticleFeed from "./components/ArticleFeedComponents/ArticleFeed";
--- src/components/ArticleFeedComponents/ArticleFeed.js
+++ src/components/ArticleFeedComponents/ArticleFeed.js
@@ -2,31 +2,50 @@ import React from "react";
 import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import Feed from "./Feed";
 
+const feeds = [
+    {
+        title: "CoronaVirus might just be the end of the world, scientist claims!",
+        image: "https://images.medicinenet.com/images/newsletter/specialty/oleander-plant.jpg",
+        website: "website1.com",
+        disease: "Disease 1",
+        publishedDate: "Monday August 20, 2020",
+        summary: "osit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur"
+    },
+    {
+        title: "Curcumin might be the cause for cancer in some cancer patients?",
+        image: "https://5.imimg.com/data5/AF/EZ/DB/SELLER-8051866/curcumin-95-natural-extract-500x500.jpg",
+        website: "website2.com",
+        disease: "Disease 2",
+        publishedDate: "Sunday February 2, 2019",
+        summary: "ratione voluptatem sequi nesciunt. Neque porroesciunt. Neque porroesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur"
+    },
+    {
+        title: "Do black people with dermatitis live less? new research found that niggers lifespan is shorter!",
+        image: "https://d279m997dpfwgl.cloudfront.net/wp/2020/07/Emmanuel-1000x776.jpg",
+        website: "website3.com",
+        disease: "Disease 3",
+        publishedDate: "Tuesday October 15, 2021",
+        summary: "osit voluptatem accusantdicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur"
+    },
+];
+
+const createFeed = (feed) => {
+    return (
+        <Feed
+        title={feed.title}
+        image={feed.image}
+        website={feed.website}
+        disease={feed.disease}
+        publishedDate={feed.publishedDate}
+        bodyText={feed.summary}/>
+    )
+};
+
 class ArticleFeed extends React.Component {
     render() {
         return (
             <div className={'col'}>
-                <Feed
-                    title={"CoronaVirus might just be the end of the world, scientist claims!"}
-                    image={'https://images.medicinenet.com/images/newsletter/specialty/oleander-plant.jpg'}
-                    website={'website1.com'}
-                    disease={'Disease 1'}
-                    publishedDate={'Monday August 20, 2020'}
-                    bodyText={'osit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur'}/>
-                <Feed
-                    title={"Curcumin might be the cause for cancer in some cancer patients?"}
-                    image={'https://5.imimg.com/data5/AF/EZ/DB/SELLER-8051866/curcumin-95-natural-extract-500x500.jpg'}
-                    website={'medicenet.com'}
-                    disease={'Disease2'}
-                    publishedDate={'Thursday October 5, 2019'}
-                    bodyText={'osit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi neectetur'}/>
-                <Feed
-                    title={"Do black people with dermatitis live less? new research found that niggers lifespan is shorter!"}
-                    image={'https://d279m997dpfwgl.cloudfront.net/wp/2020/07/Emmanuel-1000x776.jpg'}
-                    website={'niggalation.com'}
-                    disease={'Disease3'}
-                    publishedDate={'Sunday February 16, 2019'}
-                    bodyText={'osit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi neectetur'}/>
+                {feeds.map(createFeed)}
             </div>
         )
     }
