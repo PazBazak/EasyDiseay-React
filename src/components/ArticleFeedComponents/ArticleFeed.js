ab71a93afbf83f3e81cca57ebc5db1b2b76d9ef9
PazBazak <efratbazak2@gmail.com>
2020-08-26 23:24:29 +0300

    added testing enviormonet for articles
    When the client succeed at fetching the articles from the API it renders the articles on the page, if it's fails for some reason (django server not be running or something else) it renders the feeds pre-made js object.

--- src/components/ArticleFeedComponents/ArticleFeed.js
+++ src/components/ArticleFeedComponents/ArticleFeed.js
@@ -5,28 +5,31 @@ const feeds = [
     {
         id: 1,
         title: "CoronaVirus might just be the end of the world, scientist claims!",
-        image: "https://images.medicinenet.com/images/newsletter/specialty/oleander-plant.jpg",
-        website: "website1.com",
+        url: 'yo',
+        img: "https://images.medicinenet.com/images/newsletter/specialty/oleander-plant.jpg",
+        source_site: {name: "website1.com"},
         disease: "Disease 1",
-        publishedDate: "Monday August 20, 2020",
+        published_date: "Monday August 20, 2020",
         summary: "osit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur"
     },
     {
         id: 2,
         title: "Curcumin might be the cause for cancer in some cancer patients?",
-        image: "https://5.imimg.com/data5/AF/EZ/DB/SELLER-8051866/curcumin-95-natural-extract-500x500.jpg",
-        website: "website2.com",
+        url: 'yo',
+        img: "https://5.imimg.com/data5/AF/EZ/DB/SELLER-8051866/curcumin-95-natural-extract-500x500.jpg",
+        source_site: {name: "website2.com"},
         disease: "Disease 2",
-        publishedDate: "Sunday February 2, 2019",
+        published_date: "Sunday February 2, 2019",
         summary: "ratione voluptatem sequi nesciunt. Neque porroesciunt. Neque porroesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur"
     },
     {
         id: 3,
         title: "Do black people with dermatitis live less? new research found that niggers lifespan is shorter!",
-        image: "https://d279m997dpfwgl.cloudfront.net/wp/2020/07/Emmanuel-1000x776.jpg",
-        website: "website3.com",
+        url: 'yo',
+        img: "https://d279m997dpfwgl.cloudfront.net/wp/2020/07/Emmanuel-1000x776.jpg",
+        source_site: {name: "website3.com"},
         disease: "Disease 3",
-        publishedDate: "Tuesday October 15, 2021",
+        published_date: "Tuesday October 15, 2021",
         summary: "osit voluptatem accusantdicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur"
     },
 ];
@@ -52,13 +55,18 @@ function ArticleFeed() {
 
     useEffect(() => {
         const fetchFeeds = async () => {
-            const response = await fetch('http://127.0.0.1:8000/api/articles/', {
-                method: 'GET'
-            });
-            const fetchedFeeds = await response.json();
-            setCurrentFeeds(fetchedFeeds);
+            try {
+                const response = await fetch('http://127.0.0.1:8000/api/articles/', {
+                    method: 'GET'
+                });
+                const fetchedFeeds = await response.json();
+                setCurrentFeeds(fetchedFeeds);
+            } catch (e) {
+                setCurrentFeeds(feeds)
+            }
         };
-        fetchFeeds().catch(e => console.log(e));
+        fetchFeeds()
+            .catch(e => console.log("error is ", e));
     }, []);
 
     return (
