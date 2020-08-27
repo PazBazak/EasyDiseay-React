33c0f918ba1a3da338b2919d5d58ae40897e917a
PazBazak <efratbazak2@gmail.com>
2020-08-27 22:38:32 +0300

    starting to re-write the code
    Added the usage of material-UI
    Started with a new template fitting our needs.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import 'jquery'
+import 'fontsource-roboto';
+import PersistentDrawerLeft from "./components/kaki";
 
 import Header from "./components/HeaderComponents/Header";
 import DiseasesSlider from "./components/DiseaseSliderComponents/DiseasesSlider";
@@ -11,13 +13,7 @@ import ArticleFeed from "./components/ArticleFeedComponents/ArticleFeed";
 function App() {
     return (
         <div>
-            <Header/>
-            <div className={'container-fluid'}>
-                <div className={'row'}>
-                    <DiseasesSlider/>
-                    <ArticleFeed />
-                </div>
-            </div>
+           <PersistentDrawerLeft />
         </div>
     );
 }
