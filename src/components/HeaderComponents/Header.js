f14193324d470d4446ee9139d5929ddf9b5b382c
PazBazak <efratbazak2@gmail.com>
2020-08-29 16:05:06 +0300

    Added DiseaseMenuButton component
    Added DiseaseMenuButton component

--- src/components/HeaderComponents/Header.js
+++ src/components/HeaderComponents/Header.js
@@ -1,11 +1,10 @@
 import React from "react";
 import clsx from "clsx";
 import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import Logo from "./Logo";
 import AppBar from "@material-ui/core/AppBar";
 import {makeStyles} from "@material-ui/core/styles";
+import DiseaseMenuButton from "./DiseaseMenuButton";
 
 const drawerWidth = 240;
 
@@ -25,43 +24,25 @@ const headerStyle = makeStyles((theme) => ({
             duration: theme.transitions.duration.enteringScreen,
         }),
     },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    hide: {
-        display: 'none',
-    },
 }));
 
 
 function Header(props) {
     const classes = headerStyle();
 
-    const openDiseaseList = () => {
-        props.setDiseaseListOpened(true);
-    };
-
     return (
-        <div>
-            <AppBar
-                position="fixed"
-                className={clsx(classes.appBar, {
-                    [classes.appBarShift]: props.diseaseListOpened,
-                })}
-            >
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        onClick={openDiseaseList}
-                        edge="start"
-                        className={clsx(classes.menuButton, props.diseaseListOpened && classes.hide)}
-                    >
-                        <MenuIcon/>
-                    </IconButton>
-                    <Logo/>
-                </Toolbar>
-            </AppBar>
-        </div>
+        <AppBar
+            position="fixed"
+            className={clsx(classes.appBar, {
+                [classes.appBarShift]: props.diseaseListOpened,
+            })}
+        >
+            <Toolbar>
+                <DiseaseMenuButton setDiseaseListOpened={props.setDiseaseListOpened}
+                                   diseaseListOpened={props.diseaseListOpened}/>
+                <Logo/>
+            </Toolbar>
+        </AppBar>
     )
 }
 
