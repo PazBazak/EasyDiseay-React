826871f4361816ea91345f8f65dc75c0f8e23acd
PazBazak <efratbazak2@gmail.com>
2020-08-29 23:20:49 +0300

    Added sticky subheaders inside the DiseaseMenu
    Added sticky subheaders inside the DiseaseMenu
    
    Still need some refactoring that will do in the future for cleaner code.

--- src/components/DiseaseSliderComponents/DiseaseList.js
+++ src/components/DiseaseSliderComponents/DiseaseList.js
@@ -1,16 +1,39 @@
 import React from "react";
 import List from "@material-ui/core/List";
 import DiseaseListItem from "./DiseaseListItem";
+import {ListSubheader} from "@material-ui/core";
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    list: {
+        width: '100%',
+        backgroundColor: theme.palette.background.paper,
+        position: 'relative',
+        overflow: 'auto',
+        padding: '0',
+    },
+    container: {
+        backgroundColor: 'inherit',
+        padding: 0,
+    },
+}));
 
 function DiseaseList() {
+    const classes = useStyles();
+
     return (
-        <div>
-            <List>
-                {['Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease'].map((text, index) => (
+        <List className={classes.list}>
+            <div className={classes.container}>
+                <ListSubheader>Diseases</ListSubheader>
+                {['Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease'].map((text, index) => (
                     <DiseaseListItem key={index} diseaseName={text + " " + index}/>
                 ))}
-            </List>
-        </div>
+                <ListSubheader>Niggers</ListSubheader>
+                {['Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease', 'Disease'].map((text, index) => (
+                    <DiseaseListItem key={index} diseaseName={text + " " + index}/>
+                ))}
+            </div>
+        </List>
     )
 }
 
