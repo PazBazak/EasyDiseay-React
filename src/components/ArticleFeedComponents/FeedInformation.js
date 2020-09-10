50c400f820cb34fb60fca4ed6997e56bd98958a7
PazBazak <efratbazak2@gmail.com>
2020-09-10 19:46:25 +0300

    removed the function and used inside anonymouse arrow function

--- src/components/ArticleFeedComponents/FeedInformation.js
+++ src/components/ArticleFeedComponents/FeedInformation.js
@@ -1,17 +1,13 @@
 import React from "react";
 
 function FeedInformation(props) {
-    function diseasesInfo(diseases) {
-        return diseases.name;
-    }
-
     return (
         <div className={'d-flex flex-row feed-info-text'}>
             <p>{props.publishedDate}</p>
             <p className={'mx-2'}>|</p>
             <p>{props.website}</p>
             <p className={'mx-2'}>|</p>
-            <p>{props.diseases.map(diseasesInfo).join(', ')}</p>
+            <p>{props.diseases.map((diseases) => diseases.name).join(', ')}</p>
             <p className={'ml-auto mr-5'}>4 min</p>
         </div>
     )
