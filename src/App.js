9fdbcd1a72e26c4d0210ab50927812e687bdd475
PazBazak <efratbazak2@gmail.com>
2020-09-14 16:56:10 +0300

    Added login page
    Added LoginPage component to load when /login route is heppening.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'fontsource-roboto';
 import MainPage from "./components/MainPage";
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import LoginPage from "./components/LoginPage";
+import { BrowserRouter as Router} from 'react-router-dom';
+import { Route, Switch } from 'react-router';
 
 function App() {
     return (
         <Router>
             <Switch>
                 <Route path={'/'} exact component={MainPage}/>
+                <Route path={'/login'} component={LoginPage} />
             </Switch>
         </Router>
     );
