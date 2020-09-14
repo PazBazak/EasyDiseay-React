2f147495aa5aab3647123f30f6bc0047d5fde4ad
PazBazak <efratbazak2@gmail.com>
2020-09-14 16:08:29 +0300

    Wrapped App with Router
    Downloaded react-router-dom
    Added BrowserRouter, Route, Switch on App.js to wrap MainPage.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'fontsource-roboto';
 import MainPage from "./components/MainPage";
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
     return (
-        <div>
-           <MainPage />
-        </div>
+        <Router>
+            <Switch>
+                <Route path={'/'} exact component={MainPage}/>
+            </Switch>
+        </Router>
     );
 }
 
