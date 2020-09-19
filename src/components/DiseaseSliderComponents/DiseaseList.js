1488c19490f9069427fbb26adfafee1c5334e8b1
PazBazak <efratbazak2@gmail.com>
2020-09-19 14:23:11 +0300

    got it to worked

--- src/components/DiseaseSliderComponents/DiseaseList.js
+++ src/components/DiseaseSliderComponents/DiseaseList.js
@@ -4,12 +4,21 @@ import DiseaseListItem from "./DiseaseListItem";
 
 
 function DiseaseList(props) {
-    const {diseases, subheader, id, followDisease, unFollowDisease} = props;
+    const {diseases, subheader, id, followDisease, unFollowDisease, followed} = props;
+
+    const getDiseases = () => {
+        return diseases.filter((disease, index) => {
+            disease.diseaseIndex = index;
+            return disease.isFollowing === followed
+        })
+    };
+
     return (
         <div>
             <ListSubheader>{subheader}</ListSubheader>
-            {diseases.map((disease) => (
+            {getDiseases().map((disease) => (
                 <DiseaseListItem key={id}
+                                 diseaseIndex={disease.diseaseIndex}
                                  disease={disease}
                                  followDisease={followDisease}
                                  unFollowDisease={unFollowDisease}/>
