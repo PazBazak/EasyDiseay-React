4d2a57f99e52965be06b805add6016073b3163f9
PazBazak <efratbazak2@gmail.com>
2020-09-29 16:44:58 +0300

    Revert "Merge pull request #17 from milimetron/ED73-Remove-scrollbar-from-diseasesMenu"
    This reverts commit d4ad62aaa15e027ab7139a9b94a2a3c48a631d1c, reversing
    changes made to 9a51c71b91c2d60569f5277f9d643a421a4f2ad5.

--- src/components/DiseaseSliderComponents/DiseaseList.js
+++ src/components/DiseaseSliderComponents/DiseaseList.js
@@ -12,6 +12,9 @@ const useStyles = makeStyles((theme) => ({
         position: 'relative',
         overflow: 'auto',
         padding: '0',
+        "&::-webkit-scrollbar": {
+            display: 'none'
+        },
     },
     container: {
         backgroundColor: 'inherit',
