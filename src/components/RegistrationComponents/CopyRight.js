33285d841dec434bb1d7300f00503e63f520cf96
PazBazak <efratbazak2@gmail.com>
2020-09-30 13:54:09 +0300

    Changed CopyRight link

--- src/components/RegistrationComponents/CopyRight.js
+++ src/components/RegistrationComponents/CopyRight.js
@@ -2,13 +2,14 @@ import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
 import React from "react";
 import Box from '@material-ui/core/Box'
+import {Link as RouterLink} from 'react-router-dom'
 
 function Copyright() {
     return (
         <Box mt={5}>
             <Typography variant="body2" color="textSecondary" align="center">
                 {'Copyright © '}
-                <Link color="inherit" href="https://material-ui.com/">
+                <Link color="inherit" to={'/'} component={RouterLink}>
                     EasyDeasy
                 </Link>
                 {' '}
