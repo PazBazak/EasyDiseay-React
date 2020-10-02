242a456e8d19634cc16c7b2dfc757e2da70dc8da
PazBazak <efratbazak2@gmail.com>
2020-10-02 11:54:39 +0300

    Migrated ArticleFeed to use Context completley

--- src/contexts/articlesContext/articlesReducer.js
+++ src/contexts/articlesContext/articlesReducer.js
@@ -1,8 +1,13 @@
-import {SET_ARTICLES} from "../types";
+import {GET_ALL_ARTICLES, SET_ARTICLES} from "../types";
 
 export default (state, action) => {
     switch (action.type) {
         case SET_ARTICLES:
+            return {
+                ...state,
+                articles: action.payload
+            };
+        case GET_ALL_ARTICLES:
             return {
                 ...state,
                 articles: action.payload
