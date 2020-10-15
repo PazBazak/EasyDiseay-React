be754955c8d964e6cf3ef3b07cce6f112b62c92d
PazBazak <efratbazak2@gmail.com>
2020-10-15 12:22:17 +0300

    Created MiniFeed

--- src/components/ArticleFeedComponents/ArticleFeed.js
+++ src/components/ArticleFeedComponents/ArticleFeed.js
@@ -1,8 +1,15 @@
-import React, {useEffect, useContext} from "react";
+import React, {useEffect, useContext, useState} from "react";
 import Feed from "./Feed";
 import {LoadingArticles} from "../utils/LoadingsTypes";
 import ArticlesContext from "../../contexts/articlesContext/articlesContext";
+import MiniFeed from "./MiniFeed";
+import {makeStyles} from "@material-ui/core/styles";
 
+const useStyle = makeStyles((theme) => ({
+    title: {
+
+    }
+}));
 
 const createFeed = (feed) => {
     return (
@@ -20,10 +27,28 @@ const createFeed = (feed) => {
     )
 };
 
+const createMiniFeed = (feed) => {
+    return (
+        <MiniFeed
+            key={feed.id}
+            id={feed.id}
+            title={feed.title}
+            url={feed.url}
+            img={feed.img}
+            website={feed.source_site}
+            diseases={feed.diseases}
+            publishedDate={feed.published_date}
+            timeToRead={feed.time_to_read}
+            summary={feed.summary}/>
+    )
+};
+
 
 function ArticleFeed({diseaseId}) {
+    const classes = useStyle();
     const articleContext = useContext(ArticlesContext);
     const {articles, fetchArticles, fetchArticlesForDisease} = articleContext;
+    const [isMinimizedView, setIsMinimizedView] = useState(true);
 
     useEffect(() => {
         if (diseaseId === undefined) {
@@ -35,7 +60,10 @@ function ArticleFeed({diseaseId}) {
 
     return (
         <div className={'col'}>
-            {articles.length > 0 ? articles.map(createFeed) : <LoadingArticles/>}
+            {articles.length > 0 ?
+                (isMinimizedView ? articles.map(createMiniFeed) : articles.map(createFeed))
+                :
+                <LoadingArticles/>}
         </div>
     )
 }
