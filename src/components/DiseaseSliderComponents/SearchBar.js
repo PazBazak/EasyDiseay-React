aa51b50f87f0b2e8bac269ea6242b16eb92dc99b
omerpessach <omerpessach7@gmail.com>
2020-10-16 18:09:15 +0300

    Maked the searchbar strached when selected.
    This code copied completely from material UI

--- src/components/DiseaseSliderComponents/SearchBar.js
+++ src/components/DiseaseSliderComponents/SearchBar.js
@@ -1,36 +1,67 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
-import {makeStyles} from "@material-ui/core/styles";
+import {makeStyles, fade} from "@material-ui/core/styles";
 import SearchIcon from "@material-ui/icons/Search";
 import InputAdornment from "@material-ui/core/InputAdornment";
+import InputBase from '@material-ui/core/InputBase';
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
+    root: {
+        flexGrow: 1,
     },
-    textField: {
-        width: 220,
-        minWidth: 50,
+    menuButton: {
+        marginRight: theme.spacing(2),
     },
-
-    cssLabel: {
-        color: 'white !important',
-    },
-
-    cssOutlinedInput: {
-        '&$cssFocused $notchedOutline': {
-            borderColor: `white!important`,
+    title: {
+        flexGrow: 1,
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
         },
-        color: 'white !important',
     },
-
-    notchedOutline: {
-        borderColor: 'white !important'
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '30ch',
+            },
+        },
     },
 }));
 
 function SearchBar(props) {
+    const classes = useStyles();
     const {setSearchInputText} = props;
 
     const handleSearchInput = async (object) => {
@@ -38,18 +69,19 @@ function SearchBar(props) {
     };
 
     return (
-        <TextField
-            placeholder="Search..."
-            type="search"
-            onChange={handleSearchInput}
-            InputProps={{
-                startAdornment: (
-                    <InputAdornment position="start">
-                        <SearchIcon/>
-                    </InputAdornment>
-                )
-            }}
-            variant="filled"/>
+        <div className={classes.search}>
+            <div className={classes.searchIcon}>
+                <SearchIcon/>
+            </div>
+            <InputBase
+                placeholder="Search…"
+                classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                }}
+                onChange={handleSearchInput}
+                inputProps={{'aria-label': 'search'}}/>
+        </div>
     )
 }
 
@@ -63,29 +95,18 @@ function SearchBarAppBar(props) {
     };
 
     return (
-        <TextField
-            id={'Search'}
-            type="search"
-            placeholder='Search...'
-            size="small"
-            variant="outlined"
-            className={classes.textField}
-            InputLabelProps={{
-                classes: {
-                    root: classes.cssLabel,
-                },
-            }}
-            InputProps={{
-                startAdornment: (
-                    <InputAdornment position="start">
-                        <SearchIcon/>
-                    </InputAdornment>
-                ),
-                classes: {
-                    root: classes.cssOutlinedInput,
-                    notchedOutline: classes.notchedOutline,
-                },
-            }}/>
+        <div className={classes.search}>
+            <div className={classes.searchIcon}>
+                <SearchIcon/>
+            </div>
+            <InputBase
+                placeholder="Search…"
+                classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                }}
+                inputProps={{'aria-label': 'search'}}/>
+        </div>
     )
 }
 
