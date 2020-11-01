9c587e9f6d0d787fb7bc84e1470189ad79e86e29
omerpessach <omerpessach7@gmail.com>
2020-11-02 01:19:19 +0200

    Bounce page fixed
    Bounce page is using semantic ui but not affects on other pages.

--- src/components/ArticleFeedComponents/Feed.js
+++ src/components/ArticleFeedComponents/Feed.js
@@ -16,6 +16,9 @@ const useStyle = makeStyles((theme) => ({
     },
     title: {
         fontSize: '1.6rem',
+        '&:hover, &:focus': {
+            cursor: 'pointer',
+        },
     },
     readMoreStyle: {
         opacity: '0.7',
@@ -30,65 +33,66 @@ const useStyle = makeStyles((theme) => ({
         width: '90%',
         fontSize: '1.15rem',
     },
+    imgStyle: {
+        '&:hover, &:focus': {
+            cursor: 'pointer',
+        },
+    },
 }));
 
 
 const altTextPreFix = "Disease and Research News - ";
 
 const Feed = (props) => {
-    const classes = useStyle();
-
     const {
-        title,
-        diseases,
-        summary,
-        img,
-        website,
-        timeToRead,
-        publishedDate,
-        likes,
-        comments
+        article,
     } = props;
+    const {img, title, summary, diseases, source_site, time_to_read, published_date} = article;
+    const classes = useStyle();
+    const dispatch = useDispatch();
+
+    const feedHasBeenClicked = () => {
+        dispatch(setSelectedArticle(article));
+    };
 
     return (
         <Paper variant={"elevation"}
                className={classes.paper}
                square
-               elevation={5}
-        >
+               elevation={5}>
             <Box display={'flex'} flexDirection={'row'}>
-                <Box>
+
+                {/*Img*/}
+                <Box onClick={feedHasBeenClicked} className={classes.imgStyle}>
                     <img src={img}
                          alt={altTextPreFix + title}
                          className={'mini-feed-image'}/>
                 </Box>
+
+                {/*Title*/}
                 <Box className={'ml-3 w-100'} display={'flex'} flexDirection={'column'}>
-                    <Box flexGrow={1}>
+                    <Box flexGrow={1} onClick={feedHasBeenClicked}>
                         <Typography variant={'h4'}
                                     component={'h3'}
-                                    className={classes.title}
-                        >{title}</Typography>
-                        <ArticleStats
-                            likes={likes}
-                            comments={comments}
-                        />
+                                    className={classes.title}>
+                            {title}</Typography>
                     </Box>
+
+                    {/*Summary*/}
                     <Box className={classes.summaryBox}>
                         <ReactReadMoreReadLess
                             charLimit={75}
                             readMoreClassName={classes.readMoreStyle}
-                            readLessClassName={classes.readMoreStyle}
-                        >
+                            readLessClassName={classes.readMoreStyle}>
                             {summary}
                         </ReactReadMoreReadLess>
                     </Box>
                     <Box>
                         <FeedInformation
                             diseases={diseases}
-                            website={website}
-                            publishedDate={publishedDate}
-                            timeToRead={timeToRead}
-                        />
+                            website={source_site}
+                            publishedDate={published_date}
+                            timeToRead={time_to_read}/>
                     </Box>
                 </Box>
             </Box>
