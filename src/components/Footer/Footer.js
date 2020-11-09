00bff4c9cabfdc942300d73566a6a4fc16a87dad
PazBazak <efratbazak2@gmail.com>
2020-11-09 13:29:12 +0200

    Added final subtitles and titles.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,8 @@ import Divider from '@material-ui/core/Divider';
 import FooterSubTitle from "./FooterSubTitle";
 import FooterTitle from "./FooterTitle";
 import StoreBadge from "./StoreBadge";
-import {APPLE_BADGE_PATH, ANDROID_BADGE_PATH} from "../utils/Constants";
+import {ANDROID_BADGE_PATH, APPLE_BADGE_PATH} from "../utils/Constants";
+import Copyright from "../RegistrationComponents/CopyRight";
 
 const useStyle = makeStyles((theme) => ({
     container: {
@@ -35,6 +36,9 @@ const Footer = () => {
                 <Grid item xs={2}>
                     <Grid container direction={'column'}>
                         <FooterTitle title={'EasyDeasy'}/>
+                        <FooterSubTitle title={'How it works'}/>
+                        <FooterSubTitle title={'About Us'}/>
+                        <FooterSubTitle title={'The Team'}/>
                     </Grid>
                 </Grid>
                 <Grid item xs={2}>
@@ -48,8 +52,7 @@ const Footer = () => {
                 <Grid item xs={2}>
                     <Grid container direction={'column'}>
                         <FooterTitle title={'Navigation'}/>
-                        <FooterSubTitle title={'About Us'}/>
-                        <FooterSubTitle title={'The Team'}/>
+                        <FooterSubTitle title={'FAQ'}/>
                         <FooterSubTitle title={'Cookie Policy'}/>
                         <FooterSubTitle title={'Terms and Conditions'}/>
                         <FooterSubTitle title={'Privacy Policy'}/>
@@ -57,17 +60,17 @@ const Footer = () => {
                 </Grid>
                 <Grid item container xs={2}>
                     <Grid item xs={2}>
-                         <Divider orientation={'vertical'} className={classes.divider}/>
+                        <Divider orientation={'vertical'} className={classes.divider}/>
                     </Grid>
                     <Grid item xs={8}>
-                         <StoreBadge
-                             src={APPLE_BADGE_PATH}
-                             url={'apple'}
-                         />
-                         <StoreBadge
-                             src={ANDROID_BADGE_PATH}
-                             url={'apple'}
-                         />
+                        <StoreBadge
+                            src={APPLE_BADGE_PATH}
+                            url={'apple'}
+                        />
+                        <StoreBadge
+                            src={ANDROID_BADGE_PATH}
+                            url={'apple'}
+                        />
                     </Grid>
                 </Grid>
             </Grid>
@@ -77,11 +80,12 @@ const Footer = () => {
                 alignItems="center"
                 className={classes.box}
             >
-                <i className="fab fa-facebook fa-3x media-icon"/>
-                <i className="fab fa-twitter fa-3x media-icon"/>
-                <i className="fab fa-linkedin fa-3x media-icon"/>
-                <i className="fab fa-google-plus-g fa-3x media-icon"/>
+                <i className="fab fa-facebook fa-2x media-icon"/>
+                <i className="fab fa-twitter fa-2x media-icon"/>
+                <i className="fab fa-linkedin fa-2x media-icon"/>
+                <i className="fab fa-google-plus-g fa-2x media-icon"/>
             </Box>
+            <Copyright topMargin={0} background={'#1b89ff'}/>
         </Paper>
     )
 };
--- src/components/RegistrationComponents/CopyRight.js
+++ src/components/RegistrationComponents/CopyRight.js
@@ -5,9 +5,9 @@ import Box from '@material-ui/core/Box'
 import {Link as RouterLink} from 'react-router-dom'
 import {APP_NAME, COPYRIGHT} from "../utils/Constants";
 
-function Copyright() {
+function Copyright({topMargin, background}) {
     return (
-        <Box mt={5}>
+        <Box mt={topMargin} style={{backgroundColor: background}}>
             <Typography variant="body2" color="textSecondary" align="center">
                 {COPYRIGHT}
                 <Link color="inherit" to={'/'} component={RouterLink}>
